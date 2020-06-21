eaa34b7403a0d37d1af80489b3bc03e1e538e9ac
Karlus44 <charles.havez@gmail.com>
2020-06-21 20:40:18 +0200

    copy folder

--- controllers/copy_file.js
+++ controllers/copy_file.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 
  const handleCopyFile = (req,res, db)=> {
   const { doc, student, auteur } = req.body;
   console.log('copy_file')
   // console.log(doc, student);
-  const lien_eleve = doc.split('/').map((value,index)=>{if (index===2) {return student} else {return value}}).join('/')
+  const lien_eleve = doc.split(path.sep).map((value,index)=>{if (index===0) {return student} else {return value}}).join(path.sep)
   // console.log(doc, lien_eleve);
 
   if (!doc || !student) {
@@ -45,38 +46,54 @@ db.select('*').from('utilisateurs').where('mail','=',student).returning('*').the
 
   }
 
-const handleCopyFolder = (req,res) => {
+const handleCopyFolder = (req,res,AWS) => {
   const { doc, student } = req.body;
   console.log('copy_folder');
   console.log(doc, student);
   const split=doc.split('/');
-  const lien_racine = split.filter((x,ind)=> ind<2).join('/')+'/'+student;
-  const lien_eleve = split.map((value,index)=>{if (index===2) {return student} else {return value}}).join('/');
+  const lien_racine = split.join(path.sep)+path.sep+student;
+  const lien_eleve = split.map((value,index)=>{if (index===0) {return student} else {return value}}).join(path.sep);
   // const nom = split[split.length-1]
 
   nom = fs.readdirSync(doc, (err) => {
                             if (err) console.log(err);
                             console.log(lien_racine + ' was created');
                           })
-  // console.log(nom)
-  // fs.mkdirSync(lien_racine, (err) => {
+
+
+  // fs.mkdirSync(lien_eleve, { recursive: true }, (err) => {
   //                           if (err) console.log(err);
-  //                           console.log(lien_racine + ' was created');
+  //                           console.log(lien_eleve + ' was created');
   //                         })
 
-  fs.mkdirSync(lien_eleve, { recursive: true }, (err) => {
-                            if (err) console.log(err);
-                            console.log(lien_eleve + ' was created');
-                          })
+  // fs.copyFile(doc+'/'+nom, lien_eleve+'/'+nom[0], (err) => {
+  //         if (err) {res.status(400).json({
+  //           status:'e',
+  //           file : {doc: doc, student: student},
+  //           message: `Problème lors de l'opération`
+  //         })};
+  //         console.log(nom + ' was copied');}
+  //     )
 
-  fs.copyFile(doc+'/'+nom, lien_eleve+'/'+nom[0], (err) => {
-          if (err) {res.status(400).json({
-            status:'e',
-            file : {doc: doc, student: student},
-            message: `Problème lors de l'opération`
-          })};
-          console.log(nom + ' was copied');}
-      )
+
+  var s3 = new AWS.S3();
+
+  //configuring parameters
+  var params = {
+    Bucket: 'cloud-cube',
+    Body : fs.createReadStream(path.join(doc,nom[0])),
+    Key : path.join(path.basename(process.env.CLOUDCUBE_URL),student,Date.now()+nom[0],nom[0])
+  };
+
+  s3.upload(params, function (err, data) {
+    //handle error
+    if (err) {
+      console.log("Error", err);
+    }
+
+    //success
+    if (data) {
+      console.log("Uploaded in:", data.Location);
 
 
   res.json({
