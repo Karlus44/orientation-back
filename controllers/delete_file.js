931bab2ea128375e80ed0e8477769210ffd19fe6
Karlus44 <charles.havez@gmail.com>
2020-06-23 18:33:16 +0200

    fixing messages

--- controllers/delete_file.js
+++ controllers/delete_file.js
@@ -65,8 +65,8 @@ const handleDeleteFolder = (req,res,AWS) => {
         Key : path.join(path.basename(process.env.CLOUDCUBE_URL),lien,nom)
 };
   s3.deleteObject(params,function(err,data){
-          if (err)    console.log(err);
-          else        console.log(file + ' was deleted');
+          if (err)    console.log('********* ERROR: 'err);
+          else        console.log(nom + ' was deleted');
       })
 
 }
